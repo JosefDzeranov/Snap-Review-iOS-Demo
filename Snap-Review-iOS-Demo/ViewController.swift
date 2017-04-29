a139c56bfa9088f5001352bc3ae7e92a892e1fc3
IO Dzeranov <iodzeranov@MacBook-Pro-IO.local>
2017-04-29 09:36:47 +0300

    Initial Commit

---
+++ Snap-Review-iOS-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snap-Review-iOS-Demo
+//
+//  Created by IO Dzeranov on 29.04.17.
+//  Copyright © 2017 IO Dzeranov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
