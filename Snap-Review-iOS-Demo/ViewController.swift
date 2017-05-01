7e044e291b44148c63c524a64fb9a0aa868eae8c
IO Dzeranov <iodzeranov@MacBook-Pro-IO.local>
2017-05-01 23:06:08 +0300

    Implementation action method touch down for button

--- Snap-Review-iOS-Demo/ViewController.swift
+++ Snap-Review-iOS-Demo/ViewController.swift
@@ -28,7 +28,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: Actions
+    @IBAction func touchDown(_ sender: Any) {
+    print("this is action method for touch down button Login")
+        
+    }
 
 }
 
